72c844dff83a79fb8081cd9206637c87a0791d5b
KathrineHammervold <kathrine.hammervold@gmail.com>
2025-02-15 17:41:52 +0100

    Updated ports

--- houseparts.py
+++ houseparts.py
@@ -3,7 +3,14 @@ import pandas as pd
 
 app = Flask(__name__)
 
+@app.route("")
+def index():
+    return "Hello world"
+
+
+
 # Read Excel file and convert to JSON
+"""
 def get_house_parts():
     df = pd.read_excel("houseparts.xlsx")
     return df.to_dict(orient="records")
@@ -11,6 +18,9 @@ def get_house_parts():
 @app.route("/get_house_parts", methods=["GET"])
 def house_parts():
     return jsonify(get_house_parts())
-
+"""
+"""
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=80,debug=True)
+"""
+app.run(host="0.0.0.0", port=80)
